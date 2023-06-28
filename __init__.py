a9c42a0b3b9b9225b82eda746e5562444034b43b
brunocami <bruno.cami@hotmail.com>
2023-06-28 02:13:11 -0300

    configurate mongodb atlas api

--- __init__.py
+++ __init__.py
@@ -1,20 +1,343 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-from flask_cors import CORS
+import pymongo
+import sys
+from flask import Flask, request, jsonify
 from config.config import Config
+from flask_pymongo import ObjectId
+from flask_cors import CORS
+import requests
+
+try:
+  client = pymongo.MongoClient(Config.MONGO.MONGO_URI)
+  
+# return a friendly error if a URI error is thrown 
+except pymongo.errors.ConfigurationError:
+  print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
+  sys.exit(1)
+
+db=client.newsdb
 
 
-
-app = Flask(__name__)
-app.config['MONGO_URI']= Config.MONGO.MONGO_URI
-app.config['SECRET_KEY'] = Config.MONGO.SECRET_KEY
-mongo = PyMongo(app)
-
-
+# FLASK CONFIGURATION
+app=Flask(__name__)
 CORS(app)
+api_key = Config.MEDIASTACK.API_KEY
 
-db = mongo.db
+# use a collection named "recipes"
+news = db["news"]
+
+# Crear el índice de texto antes de definir la función postNewsByKeyword
+news.create_index([('author', 'text'), ('title', 'text'), ('description', 'text'), ('source', 'text')], default_language='none')
+
+# ----------------------------------- POST DE NOTICIAS GENERALES -----------------------------------
+@app.route('/', methods=['POST'])
+def postNews():
+    url = "http://api.mediastack.com/v1/news"
+    # Parámetros de la solicitud
+    params = {
+        "access_key": api_key,
+        "limit": 100,
+        "sort": "published_desc"
+    }
+
+    # Realizar la solicitud GET con el encabezado de autorización
+    response = requests.get(url, headers={"Authorization": "Bearer " + api_key}, params=params)
+
+    # Verificar el código de estado de la respuesta
+    if response.status_code == 200:
+        # Procesar la respuesta JSON
+        data = response.json()
+        # Hacer algo con los datos obtenidos
+        # Iterar sobre las descripciones de las noticias
+        for data in data['data']:
+           
+            if data['image'] is not None:
+                existing_news = news.find_one({
+                    'title': data['title'],
+                    'source': data['source']
+                })
+
+                if existing_news is None:
+                    result = news.insert_one({
+                        'author': data['author'],
+                        'title': data['title'],
+                        'description': data['description'],
+                        'url': data['url'],
+                        'source': data['source'],
+                        'image': data['image'],
+                        'category': data['category'],
+                        'language': data['language'],
+                        'country': data['country'],
+                        'published_at': data['published_at'],
+                    })
+                    inserted_id = str(result.inserted_id)
+                else:
+                    inserted_id = str(existing_news['_id'])
+
+
+        return jsonify(inserted_id)
+        
+    else:
+        # Mostrar el código de estado en caso de error
+        print("Error:", response.status_code)
+
+# ----------------------------------- POST DE NOTICIAS ARGENTINAS -----------------------------------
+@app.route('/news/ar', methods=['POST'])
+def postArgNews():
+    url = "http://api.mediastack.com/v1/news"
+    # Parámetros de la solicitud
+    params = {
+        "access_key": api_key,
+        "countries": "ar",
+        "limit": 100,
+        "sort": "published_desc"
+    }
+
+    # Realizar la solicitud GET con el encabezado de autorización
+    response = requests.get(url, headers={"Authorization": "Bearer " + api_key}, params=params)
+
+    # Verificar el código de estado de la respuesta
+    if response.status_code == 200:
+        # Procesar la respuesta JSON
+        data = response.json()
+        # Hacer algo con los datos obtenidos
+        # Iterar sobre las descripciones de las noticias
+        for data in data['data']:
+            if data['image'] is not None:
+                existing_news = news.find_one({
+                    'title': data['title'],
+                    'source': data['source']
+                })
+
+                if existing_news is None:
+                    result = news.insert_one({
+                        'author': data['author'],
+                        'title': data['title'],
+                        'description': data['description'],
+                        'url': data['url'],
+                        'source': data['source'],
+                        'image': data['image'],
+                        'category': data['category'],
+                        'language': data['language'],
+                        'country': data['country'],
+                        'published_at': data['published_at'],
+                    })
+                    inserted_id = str(result.inserted_id)
+                else:
+                    inserted_id = str(existing_news['_id'])
+
+        return jsonify(inserted_id)
+        
+    else:
+        # Mostrar el código de estado en caso de error
+        print("Error:", response.status_code)
+
+
+# ----------------------------------- POST DE NOTICIAS DE TECNOLOGIA -----------------------------------
+@app.route('/news/technology', methods=['POST'])
+def postTechNews():
+    url = "http://api.mediastack.com/v1/news"
+    # Parámetros de la solicitud
+    params = {
+        "access_key": api_key,
+        "categories": "technology",
+        "languages": "es",
+        "limit": 100,
+        "sort": "published_desc"
+    }
+
+    # Realizar la solicitud GET con el encabezado de autorización
+    response = requests.get(url, headers={"Authorization": "Bearer " + api_key}, params=params)
+
+    # Verificar el código de estado de la respuesta
+    if response.status_code == 200:
+        # Procesar la respuesta JSON
+        data = response.json()
+        # Hacer algo con los datos obtenidos
+        # Iterar sobre las descripciones de las noticias
+        for data in data['data']:
+            if data['image'] is not None:
+                existing_news = news.find_one({
+                    'title': data['title'],
+                    'source': data['source']
+                })
+
+                if existing_news is None:
+                    result = news.insert_one({
+                        'author': data['author'],
+                        'title': data['title'],
+                        'description': data['description'],
+                        'url': data['url'],
+                        'source': data['source'],
+                        'image': data['image'],
+                        'category': data['category'],
+                        'language': data['language'],
+                        'country': data['country'],
+                        'published_at': data['published_at'],
+                    })
+                    inserted_id = str(result.inserted_id)
+                else:
+                    inserted_id = str(existing_news['_id'])
+
+        return jsonify(inserted_id)
+        
+    else:
+        # Mostrar el código de estado en caso de error
+        print("Error:", response.status_code)
+
+
+# FUNCION PARA TRAER DE LA API NOTICIAS EN BASE A UNA PALABRA ESPECIFICA
+
+# @app.route('/news/<keyword>', methods=['POST'])
+# def postNewsByKeyword(keyword):
+#     url = "http://api.mediastack.com/v1/news"
+#     # Parámetros de la solicitud
+#     params = {
+#         "access_key": api_key,
+#         "keywords": keyword,
+#         "limit": 10,
+#         "sort": "published_desc"
+#     }
+
+#     # Realizar la solicitud GET con el encabezado de autorización
+#     response = requests.get(url, headers={"Authorization": "Bearer " + api_key}, params=params)
+
+#     # Verificar el código de estado de la respuesta
+#     if response.status_code == 200:
+#         # Procesar la respuesta JSON
+#         data = response.json()
+#         # Hacer algo con los datos obtenidos
+#         # Iterar sobre las descripciones de las noticias
+#         for data in data['data']:
+#             if data['image'] is not None:
+#                 existing_news = news.find_one({
+#                     'title': data['title'],
+#                     'source': data['source']
+#                 })
+
+#                 if existing_news is None:
+#                     result = news.insert_one({
+#                         'author': data['author'],
+#                         'title': data['title'],
+#                         'description': data['description'],
+#                         'url': data['url'],
+#                         'source': data['source'],
+#                         'image': data['image'],
+#                         'category': data['category'],
+#                         'language': data['language'],
+#                         'country': data['country'],
+#                         'published_at': data['published_at'],
+#                     })
+#                     inserted_id = str(result.inserted_id)
+#                 else:
+#                     inserted_id = str(existing_news['_id'])
+
+#         return jsonify(inserted_id)
+#     else:
+#         # Mostrar el código de estado en caso de error
+#         print("Error:", response.status_code)
+
+# ----------------------------------- GET DE BUSQUEDA -----------------------------------
+@app.route('/news/search', methods=['GET'])
+def getNewsByKeyword():
+    keyword = request.args.get('keyword')  # Obtener la palabra clave desde los parámetros de consulta
+
+    news_list = []
+    cursor = news.find({'$text': {'$search': keyword}})
+
+    for doc in cursor:
+        news_list.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'author': doc['author'],
+            'title': doc['title'],
+            'description': doc['description'],
+            'url': doc['url'],
+            'source': doc['source'],
+            'image': doc['image'],
+            'category': doc['category'],
+            'language': doc['language'],
+            'country': doc['country'],
+            'published_at': doc['published_at']
+        })
+
+    return jsonify(news_list)
+
+
+# ----------------------------------- GET DE TODAS LA NOTICIAS -----------------------------------
+
+@app.route('/', methods=['GET'])
+def getNews():
+    news_list = []
+    for doc in news.find():
+        news_list.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'author': (doc['author']),
+            'title': (doc['title']),
+            'description':(doc['description']),
+            'url': (doc['url']),
+            'source': (doc['source']),
+            'image': (doc['image']),
+            'category': (doc['category']),
+            'language': (doc['language']),
+            'country': (doc['country']),
+            'published_at': (doc['published_at']),
+        })
+    return jsonify(news_list)
+
+
+# ----------------------------------- GET DE NOTICIAS ARGENTINAS -----------------------------------
+@app.route('/news/ar', methods=['GET'])
+def getArgNews():
+    news_list = []
+    for doc in news.find():
+        if doc['country'] == 'ar':
+            news_list.append({
+                '_id': str(ObjectId(doc['_id'])),
+                'author': (doc['author']),
+                'title': (doc['title']),
+                'description':(doc['description']),
+                'url': (doc['url']),
+                'source': (doc['source']),
+                'image': (doc['image']),
+                'category': (doc['category']),
+                'language': (doc['language']),
+                'country': (doc['country']),
+                'published_at': (doc['published_at']),
+            })
+    return jsonify(news_list)
+
+# ----------------------------------- GET DE NOTICIAS DE TECNOLOGIA -----------------------------------
+@app.route('/news/technology', methods=['GET'])
+def getTechNews():
+    news_list = []
+    for doc in news.find():
+        if doc['category'] == 'technology':
+            news_list.append({
+                '_id': str(ObjectId(doc['_id'])),
+                'author': (doc['author']),
+                'title': (doc['title']),
+                'description':(doc['description']),
+                'url': (doc['url']),
+                'source': (doc['source']),
+                'image': (doc['image']),
+                'category': (doc['category']),
+                'language': (doc['language']),
+                'country': (doc['country']),
+                'published_at': (doc['published_at']),
+            })
+    return jsonify(news_list)
+
+# ----------------------------------- GET ONE NEWS -----------------------------------
+@app.route('/news/<string:id>', methods=['GET'])
+def getOneNews(id):
+    one_news = news.find_one({'_id': ObjectId(id)})
+    if one_news is not None:
+        one_news['_id'] = str(one_news['_id'])
+        return jsonify(one_news)
+    else:
+        return jsonify({'message': 'Noticia no encontrada'}), 404
 
 
 
 
+if __name__ == "__main__":
+    app.run(debug=True, port=4000)
--- config/config.py
+++ config/config.py
@@ -2,9 +2,13 @@
 class MediastackConfig:
     API_KEY='485e912e0b893b9a50bbad8862751035'
 
-class MongoUri: 
-    MONGO_URI='mongodb+srv://newsportaldb:<password>@news.u7dacnl.mongodb.net/?retryWrites=true&w=majority'
+class FlaskConfig:
     SECRET_KEY="8X3R2D7Z6A1B0C9H5K4"
+
+class MongoUri: 
+    MONGO_URI='mongodb+srv://newsportaldb:EmepM$yf8Qsvi82@news.u7dacnl.mongodb.net/?retryWrites=true&w=majority'
+    # MONGO_URI='mongodb://localhost:27017/prueba'
+    
 
 class OpenAiConfig:
     OPENAI_API_KEY='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'
@@ -14,3 +18,4 @@ class Config:
     MEDIASTACK=MediastackConfig
     OPENAI= OpenAiConfig
     MONGO=MongoUri
+    FLASK=FlaskConfig
